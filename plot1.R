50cf98cfff2f8164ad56a427e65d35dd1e44e1ad
dk <dk.promsoft@mail.ru>
2014-05-10 03:35:31 +0700

    cleanup script was created (see 1.R)

---
+++ plot1.R
@@ -0,0 +1,32 @@
+if(!file.exists("data")) {
+  dir.create("data")
+}
+
+fn.tidy <- 'data/df.tidy.Rda'
+
+if(!file.exists(fn.tidy)) {
+  date.from <- as.Date('2007-02-01', '%Y-%m-%d')
+  date.to <- as.Date('2007-02-02', '%Y-%m-%d')
+  fn <- 'data/exdata-data-household_power_consumption.zip'
+  fn.source <- 'https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip'
+  download.file(url=fn.source, destfile=fn, method='curl')
+  
+  fn.data <- 'household_power_consumption.txt'
+  data <- read.table(unz(fn, fn.data), header = TRUE, sep = ";", stringsAsFactors=F)
+  w <- mapply(function(x,y) paste(x, y) , data$Date, data$Time)
+  data$DateTime <- strptime(w, '%d/%m/%Y %H:%M:%S')
+  data$Date <- as.Date(data$Date, '%d/%m/%Y')
+  df.tidy <- subset(data, data$Date >= date.from & data$Date <= date.to)
+  rm(data)
+  rm(w)
+  df.tidy$Global_reactive_power <- as.numeric(df.tidy$Global_reactive_power)
+  df.tidy$Global_active_power <- as.numeric(df.tidy$Global_active_power)
+  df.tidy$Voltage <- as.numeric(df.tidy$Voltage)
+  df.tidy$Global_intensity <- as.numeric(df.tidy$Global_intensity)
+  df.tidy$Sub_metering_1 <- as.numeric(df.tidy$Sub_metering_1)
+  df.tidy$Sub_metering_2 <- as.numeric(df.tidy$Sub_metering_2)
+  save(df.tidy, file="data/df.tidy.Rda")
+} else {
+  load(fn.tidy)
+}
+
